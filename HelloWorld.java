a2e921a0ea76cbf8b5d15e30f61c9b68a048d896
Mohammad Tanzil Idrisi <tanzilidrisi@Mohammads-MacBook-Air.local>
2024-09-13 03:06:34 -0500

    Add years at Beloit to HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println(\"Hello, World!\");
+        System.out.println("Hello, World! I've been at Beloit for 2.5 years");
     }
 }
